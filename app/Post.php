569106359295671914d72838f4b38dc9cb0668ef
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-06-08 17:25:21 +0200

    Create model and pivot table

--- app/Post.php
+++ app/Post.php
@@ -13,6 +13,10 @@ class Post extends Model
     {
         return $this->belongsTo('App\Category');
     }
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 
 
     //Take a string as input and return a unique slug version
