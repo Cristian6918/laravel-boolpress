5627022b75e3fa9763c50630fb6c3d846cb824d4
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-06-09 22:58:32 +0200

    Router Created

---
+++ resources/js/routes.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+import HomeComponent from './pages/HomeComponent';
+import ContactsComponent from './pages/ContactsComponent';
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeComponent
+
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: ContactsComponent
+
+        }
+    ]
+});
+
+export default router;
